395fcf14af743c1e896d0a9fd0ec041f9bd08151
promiseugochukwunnanemere <promisennanemere@gmail.com>
2024-04-07 20:11:50 -0400

    adding files

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,7 +2,7 @@
 # import requests
 import os
 from dotenv import load_dotenv
-from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 load_dotenv()
 
@@ -55,5 +55,14 @@ def post_review(data_dict):
         return response.json()
     except:
         print("Network exception occurred")
+
 # def post_review(data_dict):
 # Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
