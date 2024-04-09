67b8bdc855689507b061015bfab14626df44203a
promiseugochukwunnanemere <promisennanemere@gmail.com>
2024-04-09 08:44:38 -0400

    Adding files

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,7 +1,8 @@
 import Dealers from './components/Dealers/Dealers';
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; // Corrected import statement
 import { Routes, Route } from "react-router-dom";
+import Dealer from "./components/Dealers/Dealer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
     </Routes>
   );
 }
