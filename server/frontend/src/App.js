01a7b0d66aa7b243099236ca6518fc281a3aed7b
promiseugochukwunnanemere <promisennanemere@gmail.com>
2024-04-12 16:58:51 -0400

    Adding files

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,8 +1,32 @@
+// import React from 'react';
+// import LoginPanel from './components/Login/Login';
+// import Register from './components/Register/Register';
+// import { Routes, Route } from "react-router-dom";
+// import Dealers from './components/Dealers/Dealers';
+// import Dealer from "./components/Dealers/Dealer"
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/login" element={<LoginPanel />} />
+//       <Route path="/register" element={<Register />} />
+//       <Route path="/dealers" element={<Dealers />} />
+//       <Route path="/dealer/:id" element={<Dealer />} />
+//       <Route path="/postreview/:id" element={<PostReview/>} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
 import LoginPanel from './components/Login/Login';
 import Register from './components/Register/Register';
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer";
+import PostReview from "./components/Dealers/PostReview"; 
 
 function App() {
   return (
@@ -10,7 +34,8 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers />} />
-      <Route path="/dealer/:id" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} /> 
     </Routes>
   );
 }
